6eb667a86b8d55b99e731a4fbab8a0ba20461871
Rich Hornung <hornung1@llnl.gov>
2026-05-22 10:17:22 -0700

    Add consistency to comma and spaces in new tests

--- test/extend.cpp
+++ test/extend.cpp
@@ -12,10 +12,10 @@ using namespace camp;
 using list1 = list<float, double, double>;
 using list2 = list<int, int, char>;
 
-CAMP_CHECK_TSAME((extend<list1,list2>), (list<float, double, double, int, int, char>));
-CAMP_CHECK_TSAME((extend<list2,list1>), (list<int, int, char, float, double, double>));
-CAMP_CHECK_TSAME((extend<list<int>,list<int>>), (list<int,int>));
-CAMP_CHECK_TSAME((extend<list<char,float>,list<>>), (list<char,float>));
-CAMP_CHECK_TSAME((extend<list<>,list<char,float>>), (list<char,float>));
-CAMP_CHECK_TSAME((extend<list<>,list1>), (list1));
-CAMP_CHECK_TSAME((extend<list2,list<>>), (list2));
+CAMP_CHECK_TSAME((extend<list1, list2>), (list<float, double, double, int, int, char>));
+CAMP_CHECK_TSAME((extend<list2, list1>), (list<int, int, char, float, double, double>));
+CAMP_CHECK_TSAME((extend<list<int>, list<int>>), (list<int, int>));
+CAMP_CHECK_TSAME((extend<list<char, float>, list<>>), (list<char, float>));
+CAMP_CHECK_TSAME((extend<list<>, list<char, float>>), (list<char, float>));
+CAMP_CHECK_TSAME((extend<list<>, list1>), (list1));
+CAMP_CHECK_TSAME((extend<list2, list<>>), (list2));
--- test/prepend.cpp
+++ test/prepend.cpp
@@ -12,10 +12,10 @@ using namespace camp;
 using list1 = list<float, double, double>;
 using list2 = list<int, int, char>;
 
-CAMP_CHECK_TSAME((prepend<list1,list2>), (list<list<int, int, char>, float, double, double>));
-CAMP_CHECK_TSAME((prepend<list2,list1>), (list<list<float, double, double>, int, int, char>));
+CAMP_CHECK_TSAME((prepend<list1, list2>), (list<list<int, int, char>, float, double, double>));
+CAMP_CHECK_TSAME((prepend<list2, list1>), (list<list<float, double, double>, int, int, char>));
 
-CAMP_CHECK_TSAME((prepend<list<int>,int>), (list<int, int>));
-CAMP_CHECK_TSAME((prepend<list1,int>), (list<int, float, double, double>));
-CAMP_CHECK_TSAME((prepend<list<>,int>), (list<int>));
-CAMP_CHECK_TSAME((prepend<list<>,list<char,int>>), (list<list<char,int>>));
+CAMP_CHECK_TSAME((prepend<list<int>, int>), (list<int, int>));
+CAMP_CHECK_TSAME((prepend<list1, int>), (list<int, float, double, double>));
+CAMP_CHECK_TSAME((prepend<list<>, int>), (list<int>));
+CAMP_CHECK_TSAME((prepend<list<>, list<char, int>>), (list<list<char, int>>));
