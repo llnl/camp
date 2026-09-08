ff653cd3be592620235be3a52264bade98bee922
Brandon Echols <echols2@llnl.gov>
2026-09-08 15:04:54 -0700

    Removes warning due to not being available on all platforms

--- include/camp/init_helpers.hpp
+++ include/camp/init_helpers.hpp
@@ -16,10 +16,6 @@
 
 namespace camp
 {
-#if !ATOMIC_BOOL_LOCK_FREE
-#warning "std::atomic<bool> is not lock free. This may impact performance."
-#endif
-
 /// Resettable version of std::once_flag
 ///
 /// This is similar to std::once_flag used in std::call_once. However,
