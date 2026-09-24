23c26c0685dee25e2f7c225cc85b68adbec56a69
Brandon Echols <echols2@llnl.gov>
2026-09-24 10:42:12 -0700

    Adds comments

--- include/camp/resource/hip.hpp
+++ include/camp/resource/hip.hpp
@@ -219,6 +219,10 @@ namespace resources
     {
       static constexpr int num_streams = 16;
 
+      // Not destroying these by default due to unspecified cleanup
+      // order of static objects. `camp::OptionalDtorPolicy::Default`
+      // can result in crashes after main due to the HIP runtime cleaning
+      // up prior to static camp streams.
       template <typename T>
       using singleton_t = camp::optional_singleton<T, camp::OptionalDtorPolicy::None>;
 
